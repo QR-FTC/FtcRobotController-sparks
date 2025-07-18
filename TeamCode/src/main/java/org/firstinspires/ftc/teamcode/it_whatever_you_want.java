e2e3206225ba9d91520ee22ba70746d92a9e191d
Aadvik <aadvikgupta007@gmail.com>
2025-07-17 20:17:27 -0700

    returns volume of a rectangular prism

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/it_whatever_you_want.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class it_whatever_you_want {
+
+    public static int findVolume (int a, int b, int c){
+        return a*b*c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The volume is " + findVolume(4,5,6));
+    }
+}
