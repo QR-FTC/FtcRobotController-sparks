21d564334539507586f11bc96bbb55cd6a81f4a7
jigipatel7048 <patelevan7048@gmail.com>
2025-07-17 20:23:41 -0700

    I made the volume multiply by 4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/doorknob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/doorknob.java
@@ -8,12 +8,20 @@ public class doorknob {
             double volume = length * width * height;
             return volume;
         }
-        
+        public static class multiplyvolumeby4{
+            public static double multiplyvolumeby4(double volume){
+                double newvolume = volume * 4;
+                return newvolume;
+            }
+        }
+
         public static void main(String[] args) {
             double length = 5.0;
             double width = 3.0;
             double height = 2.0;
             double volume = returnvolumeofrectangularprism(length, width, height);
+            System.out.println("The volume of the rectangular prism is: " + volume);
+            System.out.println("the volume multiplied by 4 is " + multiplyvolumeby4.multiplyvolumeby4(volume));
         }
     }
 }
