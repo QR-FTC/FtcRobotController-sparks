8651ac7971d36eb1b9680a2e85df4bb06fcc0aee
Aadvik <aadvikgupta007@gmail.com>
2025-07-17 19:54:46 -0700

    edited code with el commento

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/boosta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/boosta.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode;
 public class boosta {
     public static void main(String[] args) {
         int a = 4;
-    }
+    } //boosta
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/desk.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/desk.java
@@ -8,3 +8,5 @@ public class desk  {
     // change
 }
 // good job
+
+// el desko
