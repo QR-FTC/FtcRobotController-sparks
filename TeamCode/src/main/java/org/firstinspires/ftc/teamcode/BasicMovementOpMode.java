d9b28ae9ffa993c3c146f9ebb9bcfc46da7f6178
Aadvik <aadvikgupta007@gmail.com>
2025-08-14 19:05:16 -0700

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovementOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicMovementOpMode.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Basic Movement", group="Linear OpMode")
 
 public class BasicMovementOpMode extends LinearOpMode {
     private DcMotor leftMotor;
