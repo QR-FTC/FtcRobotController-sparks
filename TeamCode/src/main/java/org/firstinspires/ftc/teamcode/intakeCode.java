a0da488832cd33ea295243f2ede07f3b9de679bd
Aadvik <aadvikgupta007@gmail.com>
2025-10-30 19:45:16 -0700

    Added gecko wheels code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeCode.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
-public class intakeCode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class intakeCode extends LinearOpMode {
+    private DcMotor geckoWheels = null;
+    public void runOpMode() {
+        geckoWheels = hardwareMap.get(DcMotor.class, "Intake_1");
+        while (opModeIsActive()) {
+            waitForStart();
+            if(gamepad1.left_trigger <1)
+                geckoWheels.setPower(0.50);
+            if(gamepad1.right_trigger<1)
+                geckoWheels.setPower(-0.50);
+
+
+    }
+
+
 }
+
