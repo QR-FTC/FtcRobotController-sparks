ef56883da1672c1bc04d0907482338cc252694d0
Aadvik <aadvikgupta007@gmail.com>
2025-10-30 20:12:29 -0700

    Deposit code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deposit_1.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import static java.lang.reflect.Array.get;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class deposit_1 extends LinearOpMode {
+    private Servo servoDeposit;
+
+
+    @Override
+    public void runOpMode()  {
+        servoDeposit = get(Servo.class "deposit_1");
+
+        while (opModeIsActive()) {
+
+            waitForStart();
+
+
+            if(gamepad1.dpad_down == true )
+                servoDeposit.setPosition(0.5);
+            if(gamepad1.dpad_up == true)
+                servoDeposit.setPosition(0.0);
+
+
+
+
+
+        }
+
+    }
+}
